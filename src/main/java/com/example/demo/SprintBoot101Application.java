6889bd72bc82e8c00e3ab03c0d56b027d0234e24
EricaGleaton <EricaGleaton@comcast.net>
2019-08-26 11:07:47 -0400

    Hello World 101

---
+++ src/main/java/com/example/demo/SprintBoot101Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBoot101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBoot101Application.class, args);
+	}
+
+}
